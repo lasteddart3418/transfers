088896c8ef6028ff46918030c26a172f2289636f
Daniel <149642069+lasteddart3418@users.noreply.github.com>
2024-10-21 19:01:21 -0400

    Update pos.py

--- pos/pos.py
+++ pos/pos.py
@@ -35,7 +35,7 @@ class pos(commands.Cog, name="pos"):
     def __init__(self, bot):
         self.bot = bot
 
- @commands.command()
+    @commands.command()
     @checks.thread_only()
     @checks.has_permissions(PermissionLevel.SUPPORTER)
     async def transfer(self, ctx, *, to: str=None):
