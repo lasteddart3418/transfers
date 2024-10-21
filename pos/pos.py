967191bb3175e2712414046c54e61761725c582c
Daniel <149642069+lasteddart3418@users.noreply.github.com>
2024-10-20 23:39:20 -0400

    Create pos.py

---
+++ pos/pos.py
@@ -0,0 +1,105 @@
+import discord
+from datetime import datetime
+from discord.ext import commands
+
+
+from core import checks
+from core.models import PermissionLevel
+
+options_menu="You have provided invalid dept code.\n\n`general` - General Support\n`pr` - PR Support\n`pos` - AdvancePOS Support\n"
+
+DEPS_DATA = {
+    "general": {
+        "category_id": 1297495563390160919 ,
+        "pretty_name": "General Support,",
+        "reminders": "None",
+        "role_id": 1249750181243519097,
+        "send_message_to_user": True
+    },
+    "pr": {
+        "category_id": 1297567285493633115 ,
+        "pretty_name": "PR Support",
+        "reminders": "None",
+        "role_id": 1297394123762040932,
+        "send_message_to_user": True
+    },
+    "pos": {
+        "category_id": 1297567756320772157 ,
+        "pretty_name": "AdvancePOS Support",
+        "reminders": "None",
+        "role_id": 1269698132820758582,
+        "send_message_to_user": True
+}
+class pos(commands.Cog, name="pos"):
+    def __init__(self, bot):
+        self.bot = bot
+        
+        
+       
+    @commands.command()
+    @checks.thread_only()
+    @checks.has_permissions(PermissionLevel.SUPPORTER)
+    async def transfer(self, ctx, *, to: str=None):
+        """Command that transfers thread to other departments."""
+        if to is None:
+            embed = discord.Embed(title=f"Department Transfer", description=options_menu,
+                                  color=discord.Color.red(), timestamp=datetime.utcnow())
+            return await ctx.send(embed=embed)
+        to = to.lower()
+        data = None
+        try:
+            data = DEPS_DATA[to]
+        except:
+            embed = discord.Embed(title=f"Department Transfer",description=options_menu,
+                                  color=discord.Color.red(), timestamp=datetime.utcnow())
+            await ctx.send(embed=embed)
+            return
+
+        if data["send_message_to_user"]:
+            mes = "You are being transferred to **`"
+            mes += data["pretty_name"]
+            mes += "`**.\n"
+            mes += "Please remain __patient__ while we find a suitable staff member to assist in your request.\n\n"
+            
+            if data["reminders"] is not None:
+                mes += "**__Reminders__**\n"
+                mes += data["reminders"]
+
+            msg = ctx.message
+            msg.content = mes
+            
+            await ctx.thread.reply(msg, anonymous = False)
+        
+        await ctx.channel.edit(category=self.bot.get_channel(data["category_id"]), sync_permissions=True) 
+        await ctx.send("<@&%s>" % str(data["role_id"]))
+
+    @commands.command()
+    @checks.thread_only()
+    @checks.has_permissions(PermissionLevel.SUPPORTER)
+    async def stransfer(self, ctx, to: str=None):
+        """Silently transfers thread"""
+        if to is None:
+            embed = discord.Embed(title=f"Silent Transfer", description=options_menu,
+                                  color=discord.Color.red(), timestamp=datetime.utcnow())
+            return await ctx.send(embed=embed)
+        to = to.lower()
+        data = None
+        try:
+            data = DEPS_DATA[to]
+        except:
+            embed = discord.Embed(title=f"Silent Transfer",description=options_menu,
+                                  color=discord.Color.red(), timestamp=datetime.utcnow())
+            await ctx.send(embed=embed)
+            return
+
+        await ctx.channel.edit(category=self.bot.get_channel(data["category_id"]), sync_permissions=True) 
+        await ctx.send("Silent Transfer - <@&%s>" % str(data["role_id"]))
+
+    @commands.command()
+    @checks.thread_only()
+    @checks.has_permissions(PermissionLevel.SUPPORTER)
+    async def id(self, ctx):
+        await ctx.send(ctx.thread.id)
+
+async def setup(bot):
+    await bot.add_cog(pos(bot))
