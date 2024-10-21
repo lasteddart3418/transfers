cc3023775072c3f69fc69d40454ec7dab86af16d
Daniel <149642069+lasteddart3418@users.noreply.github.com>
2024-10-20 23:14:17 -0400

    Create coco.py

---
+++ coco/coco.py
@@ -0,0 +1,114 @@
+import discord
+from datetime import datetime
+from discord.ext import commands
+
+
+from core import checks
+from core.models import PermissionLevel
+
+options_menu="You have provided invalid dept code.\n\n`appeals` - Appeals Team\n`dev` - Development Team\n`pr` - Public Relations\n`ownership` - Ownership Team\n"
+
+DEPS_DATA = {
+    "appeals": {
+        "category_id": 1297412152226877543 ,
+        "pretty_name": "Appeals Team,",
+        "reminders": "None",
+        "role_id": 1275993530795298846,
+        "send_message_to_user": True
+    },
+    "dev": {
+        "category_id": 1297394123762040932 ,
+        "pretty_name": "Development Team",
+        "reminders": "None",
+        "role_id": 1274200868962701372,
+        "send_message_to_user": True
+    },
+    "pr": {
+        "category_id": 1294864973251612774 ,
+        "pretty_name": "Public Relations",
+        "reminders": "None",
+        "role_id": 1294502396252262492,
+        "send_message_to_user": True
+    },
+    "ownership": {
+        "category_id": 1297036892402880573 ,
+        "pretty_name": "Ownership Team",
+        "reminders": "None",
+        "role_id": 1274200868962701374,
+        "send_message_to_user": True
+    },
+
+}
+class coco(commands.Cog, name="Coco Main Commands"):
+    def __init__(self, bot):
+        self.bot = bot
+        
+        
+       
+    @commands.command()
+    @checks.thread_only()
+    @checks.has_permissions(PermissionLevel.SUPPORTER)
+    async def transfer(self, ctx, *, to: str=None):
+        """Command that transfers thread to other departments."""
+        if to is None:
+            embed = discord.Embed(title=f"Department Transfer", description=options_menu,
+                                  color=discord.Color.red(), timestamp=datetime.utcnow())
+            return await ctx.send(embed=embed)
+        to = to.lower()
+        data = None
+        try:
+            data = DEPS_DATA[to]
+        except:
+            embed = discord.Embed(title=f"Department Transfer",description=options_menu,
+                                  color=discord.Color.red(), timestamp=datetime.utcnow())
+            await ctx.send(embed=embed)
+            return
+
+        if data["send_message_to_user"]:
+            mes = "You are being transferred to **`"
+            mes += data["pretty_name"]
+            mes += "`**.\n"
+            mes += "Please remain __patient__ while we find a suitable staff member to assist in your request.\n\n"
+            
+            if data["reminders"] is not None:
+                mes += "**__Reminders__**\n"
+                mes += data["reminders"]
+
+            msg = ctx.message
+            msg.content = mes
+            
+            await ctx.thread.reply(msg, anonymous = False)
+        
+        await ctx.channel.edit(category=self.bot.get_channel(data["category_id"]), sync_permissions=True) 
+        await ctx.send("<@&%s>" % str(data["role_id"]))
+
+    @commands.command()
+    @checks.thread_only()
+    @checks.has_permissions(PermissionLevel.SUPPORTER)
+    async def stransfer(self, ctx, to: str=None):
+        """Silently transfers thread"""
+        if to is None:
+            embed = discord.Embed(title=f"Silent Transfer", description=options_menu,
+                                  color=discord.Color.red(), timestamp=datetime.utcnow())
+            return await ctx.send(embed=embed)
+        to = to.lower()
+        data = None
+        try:
+            data = DEPS_DATA[to]
+        except:
+            embed = discord.Embed(title=f"Silent Transfer",description=options_menu,
+                                  color=discord.Color.red(), timestamp=datetime.utcnow())
+            await ctx.send(embed=embed)
+            return
+
+        await ctx.channel.edit(category=self.bot.get_channel(data["category_id"]), sync_permissions=True) 
+        await ctx.send("Silent Transfer - <@&%s>" % str(data["role_id"]))
+
+    @commands.command()
+    @checks.thread_only()
+    @checks.has_permissions(PermissionLevel.SUPPORTER)
+    async def id(self, ctx):
+        await ctx.send(ctx.thread.id)
+
+async def setup(bot):
+    await bot.add_cog(Coco(bot))
